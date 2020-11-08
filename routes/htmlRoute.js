1397525b54c1b6fbce076b2abaa603bfea647313
codyalmand <codybalmand@gmail.com>
2020-11-08 16:12:18 -0600

    fixed bugs, now back to 100% functional

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,12 +1,12 @@
-// Dependencies
+
 const path = require("path");
-// Directories executing script
+
 module.exports = function (app) {
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     
     app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 //  PORT number
 const PORT = process.env.PORT || 8080;
-// Requires html files
+// Requires each of the route files for use on the server
 require("./routes/apiRoute")(app);
 require("./routes/htmlRoute")(app);
 // Path for running node express from directory
